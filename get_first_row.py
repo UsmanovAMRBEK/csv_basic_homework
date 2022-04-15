4b861b436948f70e28b54330ce360f877c2ce36d
unknown <princariens@gmail.com>
2022-04-15 09:07:57 +0500

    05

--- get_first_row.py
+++ get_first_row.py
@@ -7,6 +7,10 @@ def get_first_row(data):
         list: First row.
    """
    
-   return
+   return data.split('\n')[0].split(',')
 
 # Read the csv file
+f=open('data.csv','r')
+data=f.read()
+print(get_first_row(data))
+f.close()
