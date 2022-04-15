adbe318d6039c00b20951f7193b74317e8d7c89a
unknown <princariens@gmail.com>
2022-04-15 09:06:46 +0500

    03

--- get_column_names.py
+++ get_column_names.py
@@ -7,6 +7,10 @@ def get_column_names(data):
     Returns:
         column_names: list of column names
     """
-    return
+    return data.split('\n')[0].split(',')
     
 # Read the csv file
+f=open('data.csv','r')
+data=f.read()
+print(get_column_names(data))
+f.close()
